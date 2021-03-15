ec0681c00ebb33b271836ef8600e8eac16e96f2d
Carson <dragonchen66@gmail.com>
2021-03-15 13:51:51 -0400

    fix: prod env variables

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const SpeedMeasureWebpackPlugin = require('speed-measure-webpack-plugin');
 const smp = new SpeedMeasureWebpackPlugin();
 const { merge } = require('webpack-merge');
@@ -6,6 +7,12 @@ const prodConfig = {
   output: {
     filename: '[name].js',
   },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+      DEBUG: false,
+    }),
+  ],
   mode: 'production',
 };
 
