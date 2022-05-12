f400e4d72e26f5e44fe3b0c4d5fe3eca6010a91b
Carson <dragonchen66@gmail.com>
2022-05-12 13:38:01 -0400

    feat: add time utils

---
+++ src/utils/time.js
@@ -0,0 +1,132 @@
+import is from './is';
+
+class Time {
+
+  /**
+   * Time helpers - get hours from a value in seconds
+   * @param {number} value
+   * @return {number}
+   */
+  static getHours(value) {
+    Math.trunc((value / 60 / 60) % 60);
+  }
+
+  /**
+   * Time helpers - get minutes from a value in seconds
+   * @param {number} value
+   * @return {number}
+   */
+  static getMinutes(value) {
+    Math.trunc((value / 60) % 60);
+  }
+
+  /**
+   * Time helpers - get seconds from a value in seconds
+   * @param {number} value
+   * @return {number}
+   */
+  static getSeconds(value) {
+    Math.trunc(value % 60);
+  }
+
+  /**
+   * Format time to UI friendly string
+   * @param {number|null} time
+   * @param {boolean} displayHours - whether displaying hours number
+   * @param {boolean} inverted - add a '-' if inverted
+   * @return {string}
+   */
+  static formatTime(time = 0, displayHours = false, inverted = false) {
+    // Bail if the value isn't a number
+    if (!is.number(time)) {
+      return Time.formatTime(null, displayHours, inverted);
+    }
+
+    // Format time component to add leading zero
+    const format = value => `0${value}`.slice(-2);
+    // Breakdown to hours, mins, secs
+    let hours = Time.getHours(time);
+    const mins = Time.getMinutes(time);
+    const secs = Time.getSeconds(time);
+
+    // Do we need to display hours?
+    if (displayHours || hours > 0) {
+      hours = `${hours}:`;
+    } else {
+      hours = '';
+    }
+
+    // Render
+    return `${inverted && time > 0 ? '-' : ''}${hours}${format(mins)}:${format(
+      secs)}`;
+  }
+
+  /**
+   * Add leading zeros
+   * @param {string|number} num
+   * @param {number} length
+   * @return {string}
+   */
+  static leadingZeros(num, length) {
+    const digitLength = length || 2;
+    const digit = String(num);
+    if (digit.length > digitLength) {
+      return digit;
+    }
+    return (Array(digitLength + 1).join('0') + digit).substr(-digitLength);
+  }
+
+  /**
+   * @param {number} maxAge
+   * @param {number|string} timestamp
+   * @return {boolean}
+   */
+  static isExpired(maxAge, timestamp) {
+    const now = +new Date();
+    let aTimestamp = timestamp;
+
+    if (is.string(timestamp)) {
+      aTimestamp = parseInt(timestamp, 10);
+    }
+
+    return now - aTimestamp >= maxAge;
+  }
+
+  /**
+   * @param {number} ms
+   */
+  static wait(ms = 300) {
+    return new Promise(res => {
+      window.setTimeout(res, ms);
+    });
+  }
+
+  /**
+   * @param {CallableFunction} conditionFn
+   * @param {number} [interval]
+   * @param {number} [maxTry]
+   * @return {Promise<boolean>}
+   */
+  static async waitForReady(conditionFn, interval = 100, maxTry = 50) {
+    let isReady = conditionFn();
+
+    if (isReady) {
+      return true;
+    }
+
+    for (let i = 0; i < maxTry; i += 1) {
+      if (isReady) {
+        break;
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      await Time.wait(interval);
+      isReady = conditionFn();
+    }
+
+    return isReady;
+  }
+
+}
+
+export default Time;
