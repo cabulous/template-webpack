2381d67984cf178f29bc16a54ba4521dfb354039
Carson <dragonchen66@gmail.com>
2022-06-09 12:40:48 -0400

    feat: add new util methods

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -59,3 +59,27 @@ export function redirectTo(url) {
 
   window.location.replace(url);
 }
+
+/**
+ * sends a request to the specified url from a form. this will change the window location.
+ * @param {string} path the path to send the post request to
+ * @param {object} params the parameters to add to the url
+ * @param {string} [method=post] the method to use on the form
+ */
+export function postInHiddenForm(path, params, method = 'post') {
+  const form = document.createElement('form');
+  form.method = method;
+  form.action = path;
+
+  Object.keys(params).forEach(key => {
+    const hiddenField = document.createElement('input');
+    hiddenField.type = 'hidden';
+    hiddenField.name = key;
+    hiddenField.value = params[key];
+
+    form.appendChild(hiddenField);
+  });
+
+  document.body.appendChild(form);
+  form.submit();
+}
